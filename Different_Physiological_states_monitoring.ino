963382d89b07f221a5522c28635b3346c63f915d
AbdAlfatah <abdalfatah2000gg@gmail.com>
2022-08-28 11:53:30 +0200

    2nd_version

--- Different_Physiological_states_monitoring.ino
+++ Different_Physiological_states_monitoring.ino
@@ -1,5 +1,5 @@
 #include <Wire.h>
-#include <WiFi.h>
+#include <WiFiManager.h>
 #include <SHT21.h>
 #include <Adafruit_Sensor.h>
 #include "time.h"
@@ -8,8 +8,6 @@
 #include <addons/TokenHelper.h>
 SHT21 sht;
 bool ECGisON = false;
-bool old_BTN_state;
-
 // Insert Firebase project API Key
 #define API_KEY "AIzaSyAv4xg6FeU-n1PCmfqLy91l6q3q3l3MMw4"
 
@@ -20,10 +18,6 @@ bool old_BTN_state;
 // Insert Authorized Email and Corresponding Password
 #define USER_EMAIL "ua3agw@stud.uni-obuda.hu"
 #define USER_PASSWORD "777316125@"
-
-//Insert the Wifi ssid and the passward of the provided network
-#define WIFI_SSID "Redmi Note 9"
-#define WIFI_PASSWORD "Yemen7777"
 
 /////////////////////////////////////////////////////////////////
 //initializing the parameters for sending the data
@@ -57,19 +51,17 @@ const char* ntpServer = "pool.ntp.org";
 #define LED1 17
 #define LED2 18
 #define LED3 19
+#define LED4 4
 
 #include "MAX30100_PulseOximeter.h"
 #define REPORTING_PERIOD_MS 1000
-#define LO2 25
-#define LO1 26
-#define ECG 15
 PulseOximeter pox;
 uint32_t tsLastReport = 0;
 float temp;
 float humidity;
-uint32_t delayingTime = 300;
-uint32_t delayingTime1 = 750;
-uint32_t timeRunning = 0;
+uint16_t delayingTime = 300;
+uint16_t delayingTime1 = 750;
+uint16_t timeRunning = 0;
 unsigned long sendDataPrevMillis = 0;
 unsigned long timerDelay = 5000;
  String firetemp=String("");
@@ -79,6 +71,7 @@ unsigned long timerDelay = 5000;
 
 void onBeatDetected()
 {
+  pox.update();
   digitalWrite(LED1,!digitalRead(LED1));
 }
 unsigned long getTime() {
@@ -89,16 +82,6 @@ unsigned long getTime() {
   }
   time(&now);
   return now;
-}
-void ECG_monitor() {
-  if ((digitalRead(25) == 1) || (digitalRead(26) == 1)) {
-    Serial.println('!');
-  }
-  else {
-    Serial.println(analogRead(ECG));
-  }
-  //Wait for a bit to keep serial data from saturating
-  delay(1);
 }
 void temp_and_humi() {
   temp = sht.getTemperature();  // get temp from SHT
@@ -127,20 +110,34 @@ void pulse_oximeter() {
 }
 
 void connect_to_Wifi() {
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting to ");
-  Serial.print(WIFI_SSID);
-  while (WiFi.status() != WL_CONNECTED) {
-    if (millis() - timeRunning > delayingTime) {
-      timeRunning = millis();
-      Serial.print("!.");
+      WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
+    // it is a good practice to make sure your code sets wifi mode how you want it.
+    //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wm;
+
+    // reset settings - wipe stored credentials for testing
+    // these are stored by the esp library
+    //wm.resetSettings();
+
+    // Automatically connect using saved credentials,
+    // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
+    // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
+    // then goes into a blocking loop awaiting configuration and will return success result
+
+    bool res;
+    // res = wm.autoConnect(); // auto generated AP name from chipid
+    // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
+    res = wm.autoConnect("EESP32","AbdAlfatah"); // password protected ap
+
+    if(!res) {
+        Serial.println("Failed to connect");
+        // ESP.restart();
+    } 
+    else {
+        //if you get here you have connected to the WiFi    
+        Serial.println("connected...yeey :)");
+        digitalWrite( LED2, !digitalRead(LED2));
     }
-  }
-  Serial.println();
-  Serial.print("Connected");
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());  //prints local IP address
-  digitalWrite(LED2,!digitalRead(LED2));
 }
 
 void Sending_new_readings(){
@@ -152,7 +149,7 @@ void Sending_new_readings(){
     timestamp = getTime();
     Serial.print ("time: ");
     Serial.println (timestamp);
-    //Creating a p
+    //Creating path in the database for sending the data
     parentPath= databasePath + "/" + String(timestamp);
 
     json.set(tempPath.c_str(), firetemp );
@@ -161,6 +158,7 @@ void Sending_new_readings(){
     json.set(oxiPath.c_str(), fireOxi);
     json.set(timePath, String(timestamp));
     Serial.printf("Set json... %s\n", Firebase.RTDB.setJSON(&fbdo, parentPath.c_str(), &json) ? "ok" : fbdo.errorReason().c_str());
+    digitalWrite(LED3,!digitalRead(LED3)); //THIS led will blink in case of data being sent
   }
 }
 
@@ -169,13 +167,14 @@ TaskHandle_t Task2;
 
 void setup() {
   Serial.begin(115200); 
-  pinMode(LO1 , INPUT); // Setup THE leads off detection LO -
-  pinMode(LO2 , INPUT); // Setup THE leads off detection LO +
-  pinMode(LED1,OUTPUT);
+  /*pinMode(LO1 , INPUT); // Setup THE leads off detection LO -
+  pinMode(LO2 , INPUT); // Setup THE leads off detection LO +*/
+  pinMode(LED1,OUTPUT);  //LEDs setup
   pinMode(LED2,OUTPUT);
+  pinMode(LED3,OUTPUT);
+  pinMode(LED4,OUTPUT);
   pinMode(BTN, INPUT);
-  old_BTN_state = digitalRead(BTN);
-    timeRunning = millis();
+  timeRunning = millis();
   //create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
   xTaskCreatePinnedToCore(
                    Task1code,   /* Task function. */
@@ -213,14 +212,11 @@ Serial.print("Initializing pulse oximeter..");
   pox.update();
   pox.setOnBeatDetectedCallback(onBeatDetected);
   for(;;){
-      bool new_state = digitalRead(BTN);
-    if (ECGisON == false) {
     pulse_oximeter();
     if (millis() - timeRunning > delayingTime1) {
         pox.update();
-         temp_and_humi();
+        temp_and_humi();
       timeRunning = millis();
-    }
   } 
 }
 }
@@ -265,22 +261,13 @@ void Task2code( void * pvParameters ){
   databasePath = "/UsersData/" + uid + "/readings";
 
   for(;;){
-  bool new_state = digitalRead(BTN);
-  if (digitalRead(BTN) != old_BTN_state) {
-    old_BTN_state = new_state;
-    if (LOW == new_state) {
-      ECGisON = !ECGisON;
-      Serial.println("Button is pressed now, To stop the ecg monitoring press it again");
-    }
-  }
-  if (ECGisON == true) {
-    ECG_monitor();
-  }
+
    Sending_new_readings();
+
   }
-  
 }
 
 
 void loop() {
 }
+
