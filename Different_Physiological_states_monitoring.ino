1b3e835927c2ca18585e6961cf00ba41026205a7
AbdAlfatah <abdalfatah2000gg@gmail.com>
2022-08-28 12:38:38 +0200

    version--3

--- Different_Physiological_states_monitoring.ino
+++ Different_Physiological_states_monitoring.ino
@@ -1,3 +1,12 @@
+/*IOT project:: This code a part of big project in which data being measured is sent to an outer website
+designed by the other coworkers*/
+/*Project_Name:: Daily routine planner*/
+/*Author:: Abd-Alfatah Alodainy*/
+/*E-mail:: ua3agw@stud.uni-obuda.hu*/
+/* This program is used to Run an Arduino based device in which 3  sensors are connected to a  microcontroller
+to measure the different human physiological states (HR, TEMP, HUMIDITY, O2 level and support the ECG function) */
+
+/*Necessary libraries */
 #include <Wire.h>
 #include <WiFiManager.h>
 #include <SHT21.h>
@@ -6,30 +15,30 @@
 #include <Firebase_ESP_Client.h>
 // Provide the token generation process info.
 #include <addons/TokenHelper.h>
+//Define SHT21 SENSOR objects
 SHT21 sht;
-bool ECGisON = false;
-// Firebase project API Key
+// Key of firebase project API
 #define API_KEY "AIzaSyAv4xg6FeU-n1PCmfqLy91l6q3q3l3MMw4"
 
 //Host and authentication foR the realtime database
 #define FIREBASE_HOST "dailyroutineplanner-1645e-default-rtdb.firebaseio.com"
 #define FIREBASE_AUTH "HTuTh5Rf2L26riiKdepNbRH3tUfHSt3ILgkpIKti"
 
-// Insert Authorized Email and Corresponding Password
+// Authorized Email and Corresponding Password for the user in the you entered in firebase
 #define USER_EMAIL "ua3agw@stud.uni-obuda.hu"
 #define USER_PASSWORD "777316125@"
 
 /////////////////////////////////////////////////////////////////
 //initializing the parameters for sending the data
-// Define Firebase objects
+// Define Firebase objects, like data and authentication 
 FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
 
-// Variable to save USER UID
+// Variable to save USER UID ::UID is a unique carchecters that is automatically made by firebase for every user
 String uid;
 
-// Database main path (to be updated in setup with the user UID)
+// Database main path (it is being updated in setup with the user UID)
 String databasePath;
 // Database child nodes
 String tempPath = "/temperature";
@@ -40,40 +49,48 @@ String timePath = "/timestamp";
 
 // Parent Node (to be updated in every loop)
 String parentPath;
-
+//declaration of time stamp of the Arduino running time:::: to be used in case of continous monitoring  
 int timestamp;
+//Firebase json file initializing 
 FirebaseJson json;
 
 const char* ntpServer = "pool.ntp.org";
 
 /***************************************************************************/
+//LEDs ot show different opperation signs
 #define BTN 16
-#define LED1 17
-#define LED2 18
-#define LED3 19
-#define LED4 4
-#define REPORTING_PERIOD_MS 1000
+#define LED2 17 // if it is ON then Wifi is connected
+#define LED3 18 // if it is blinking then data is being sent
+#define LED1 19 //if it is blinking then hear rate is being measured 
+#define LED4 4 // if it is ON then ECG is connected
 /**************************************************************************/
+// TIME OF HEART RATE SENISNG
+#define REPORTING_PERIOD_MS 1000
+//initializing the pulse_oximeter senosr 
 #include "MAX30100_PulseOximeter.h"
 PulseOximeter pox;
 uint32_t tsLastReport = 0;
+////initializing the temprature varilable
 float temp;
 float humidity;
+//Non blocking delays variable
 uint16_t delayingTime = 300;
 uint16_t delayingTime1 = 750;
 uint16_t timeRunning = 0;
 unsigned long sendDataPrevMillis = 0;
 unsigned long timerDelay = 5000;
+//initializing the strings for the firebase path variable 
 String firetemp=String("");
 String fireOxi =String("");
 String fireheart=String("");
 String firehum=String("");
-
+//blinking and updating the HR in case of sensing any beats from huamn finger
 void onBeatDetected()
 {
   pox.update();
   digitalWrite(LED1,!digitalRead(LED1));
 }
+//getting the timestamp of the Arduino
 unsigned long getTime() {
   time_t now;
   struct tm timeinfo;
@@ -83,34 +100,36 @@ unsigned long getTime() {
   time(&now);
   return now;
 }
+// temp and hum measuring
 void temp_and_humi() {
   if (millis() - timeRunning > 85){
   temp = sht.getTemperature();  // get temp from SHT
   humidity = sht.getHumidity(); // get temp from SHT
-  Serial.print("Temp: ");      // printing the readings to the serial output
+ /* Serial.print("Temp: ");      // printing the readings to the serial output
   Serial.print(temp);
   Serial.print("\t Humidity: ");
-  Serial.println(humidity);
+  Serial.println(humidity);*/
   firetemp = String(temp) + String("°C");
   firehum = String(humidity) + String("%");
   }
   pox.update();
 }
-
+// HR and O2 measuring 
 void pulse_oximeter() {
   pox.update();
   if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
-    Serial.print("Heart rate:");
+    // printing the readings to the serial monitor
+   /* Serial.print("Heart rate:");
     Serial.print(pox.getHeartRate());
     Serial.print("bpm / SpO2:");
     Serial.print(pox.getSpO2());
-    Serial.println("%");
+    Serial.println("%");*/
     fireheart = String(pox.getHeartRate()) + String("bpm");
     fireOxi = String ("SpO2:") + String(pox.getSpO2()) + String("%");
     tsLastReport = millis();
   }
 }
-
+//WiFi connection 
 void connect_to_Wifi() {
       WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
     // it is a good practice to make sure your code sets wifi mode how you want it.
@@ -129,7 +148,7 @@ void connect_to_Wifi() {
     bool res;
     // res = wm.autoConnect(); // auto generated AP name from chipid
     // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
-    res = wm.autoConnect("EESP32","AbdAlfatah"); // password protected ap
+    res = wm.autoConnect("ESP32","AbdAlfatah"); // password protected ap
 
     if(!res) {
         Serial.println("Failed to connect");
@@ -142,7 +161,7 @@ void connect_to_Wifi() {
         //if the LED is ON then it is connected  
     }
 }
-
+//Data transfere from the controlleres to the firebase
 void Sending_new_readings(){
 // Send new readings to database
   if (Firebase.ready() && (millis() - sendDataPrevMillis > timerDelay || sendDataPrevMillis == 0)){
@@ -150,9 +169,9 @@ void Sending_new_readings(){
 
     //Get current timestamp
     timestamp = getTime();
-    Serial.print ("time: ");
-    Serial.println (timestamp);
-    //Creating path in the database for sending the data
+    /*Serial.print ("time: ");
+    Serial.println (timestamp);*/ //print the current time stamp to the serial monitor
+    //Creating path in the database for data sending
     parentPath= databasePath + "/" + String(timestamp);
 
     json.set(tempPath.c_str(), firetemp );
@@ -178,7 +197,7 @@ void setup() {
   pinMode(LED4,OUTPUT);
   pinMode(BTN, INPUT);
   timeRunning = millis();
-  //create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
+  //This task will be executed in the Task1code() function, with priority 1 and executed on core 0
   xTaskCreatePinnedToCore(
                    Task1code,   /* Task function. */
                     "Task1",     /* name of task. */
@@ -189,7 +208,7 @@ void setup() {
                     0);          /* pin task to core 0 */                  
   delay(500); 
 
-  //create a task that will be executed in the Task2code() function, with priority 1 and executed on core 1
+  //This task will be executed in the Task2code() function, with priority 1 and executed on core 1
  xTaskCreatePinnedToCore(
                     Task2code,   /* Task function. */
                     "Task2",     /* name of task. */
@@ -201,14 +220,15 @@ void setup() {
                     );          /* pin task to core 1 */
    delay(500); 
 }
+//task 1 code (In core 0 we will run the all sensors)
 void Task1code( void * pvParameters ){
-Serial.print("Initializing pulse oximeter..");
+  //Serial printing
+//Serial.print("Initializing pulse oximeter..");
   // Initialize the PulseOximeter instance
   // Failures are generally due to an improper I2C wiring, missing power supply
   // or wrong target chip
   if (!pox.begin()) {
     Serial.println("FAILED");
-    //for (;;);
   } else {
     Serial.println("SUCCESS");
   }
@@ -223,7 +243,8 @@ Serial.print("Initializing pulse oximeter..");
   } 
 }
 }
-//Task2code: blinks an LED every 700 ms
+//Task2code: Connect to wifi and send the data to the firebase
+
 void Task2code( void * pvParameters ){
     connect_to_Wifi();
   // Assign the api key (required)
@@ -248,17 +269,17 @@ void Task2code( void * pvParameters ){
   // Initialize the library with the Firebase authen and config
   Firebase.begin(&config, &auth);
   // Getting the user UID might take a few seconds
-  Serial.println("Getting User UID");
+ // Serial.println("Getting User UID");
   while ((auth.token.uid) == "") {
     if (millis() - timeRunning > (delayingTime+250)) {
       timeRunning = millis();
-      Serial.print('#');
+      //Serial.print('#');
     }
   }
   // Print user UID
   uid = auth.token.uid.c_str();
-  Serial.print("User UID: ");
-  Serial.println(uid);
+  /*Serial.print("User UID: ");
+  Serial.println(uid);*/
 
   // Update database path
   databasePath = "/UsersData/" + uid + "/readings";
@@ -272,5 +293,9 @@ void Task2code( void * pvParameters ){
 
 
 void loop() {
+  /* Here should be empty because we used the dula core of the ESP32 in the setup
+  and there is not any need for further code here
+  
+  */
 }
-
+/* I have comment all the serial printing for testing you have to uncomment all of them*/
